707dde028370271f860266eb032abc527a3d9bb3
Devin Suy <54761385+devinsuy@users.noreply.github.com>
2020-10-03 14:17:43 -0700

    Update MiniMax.py

--- Algorithm/MiniMax.py
+++ Algorithm/MiniMax.py
@@ -1,6 +1,6 @@
-#---------------------------------
-# CECS 451 : Devin Suy : 017001983
-#---------------------------------
+#----------
+# Devin Suy
+#----------
 
 from Algorithm.GameState import GameState
 
